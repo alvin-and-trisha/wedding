be125f26181599f9f1ffb2dc5b0361596bf7da38
Welch <thisiswelch@protonmail.com>
2024-10-05 23:55:14 +0800

    Navbar with name

--- client/wedding/tailwind.config.js
+++ client/wedding/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        greatvibes: ['"Great Vibes"', 'cursive'], // Add the 'Great Vibes' font
+      }
+    },
   },
   plugins: [],
 }
